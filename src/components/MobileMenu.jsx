5ab3f862485f30746e58dea291eba84dce115dc8
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-07-31 14:15:59 +0600

    update components

--- src/components/MobileMenu.jsx
+++ src/components/MobileMenu.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import React, { useState } from "react";
+import { useState } from "react";
 import { FiMenu, FiX } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
